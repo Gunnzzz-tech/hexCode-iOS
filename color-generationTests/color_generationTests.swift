69c566b5b752191308e1e9e3cb44eceadb5b2463
gungunbali <gungunbali.hky@gmail.com>
2025-07-30 13:37:29 +0530

    Initial Commit

---
+++ color-generationTests/color_generationTests.swift
@@ -0,0 +1,17 @@
+//
+//  color_generationTests.swift
+//  color-generationTests
+//
+//  Created by Gungun Bali on 30/07/25.
+//
+
+import Testing
+@testable import color_generation
+
+struct color_generationTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
